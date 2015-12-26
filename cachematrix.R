8cf74275a11f437ddfc50ce236d6a45ae49b10f2
Doryan Gowty <Dozer@Garry.local>
2015-12-27 10:00:37 +1100

    Commiting my R Programming Assignment 2 submission, D.G. 27/12

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author: Doryan Gowty
+## R Programming, Assignment 2
+## Date: 27/12/2015
+## 
+## This program caches the inverse of a matrix
+## for retreival without having to recompute
+## results
+## 
 
-## Write a short comment describing this function
+## create list of functions that takes and stores matrix, 
+## computes inverse and stores results 
+makeCacheMatrix <- function(X = matrix()) {
+        
+        M <- matrix(,nrow= nrow(X), ncol = ncol(X) ) #Empty matrix for holding inverse 
+        
+        setMat <- function(y) {
+                X <<- y #assign input matrix
+                M <<- matrix(,nrow= nrow(X), ncol = ncol(X) ) #clear cache from inverse 
+        }#end function setMat
+        getMat <- function() X 
+        setInv <- function(invMat) M <<- invMat 
+        getInv <- function() M
+        
+        list(
+                setMat = setMat
+                , getMat = getMat
+                , setInv = setInv
+                , getInv = getInv
+        )
+} #end function makeCacheMatrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
+## Check for cached value of matrix inverse, return value
+## if there is one, otherwise calculate new value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        m <- x$getInv()
+        
+        if(!all(is.na(m))) {
+                message("retrieving cached inverse")
+                return(m)
+        }
+        
+        message("no data cached")
+        m <- solve(x$getMat())
+        x$setInv(m) # cache results
+        m 
+} #end function cacheSolve
